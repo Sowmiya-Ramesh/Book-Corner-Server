59034b75bd2bc5d6b53ff0d2cc1985fe2b67f2ee
Sowmiya-Ramesh <miyaramesh9944@gmail.com>
2023-07-22 08:59:37 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+/*
+ * File: server.js
+ * Project: <<Book Corner>>
+ * File Created: Saturday, 22nd July 2023 8:48:37 am
+ * Author: Sowmiya-Ramesh (miyaramesh9944@gmail.com)
+ * -----
+ * Last Modified: Saturday, 22nd July 2023 8:54:53 am
+ * Modified By: Sowmiya-Ramesh (miyaramesh9944@gmail.com)
+ * -----
+ * Copyright 2023 - 2023 Your Company, Your Company
+ */
